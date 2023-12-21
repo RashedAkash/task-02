007b9d87549b086872a42b09e9fe050466b409b0
hablu <131282586+hablu@users.noreply.github.com>
2023-12-21 09:25:20 +0600

    banner create

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 const MainLayouts = () => {
   return (
     <div className=' max-w-5xl mx-auto'>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
